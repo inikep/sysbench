20696789f0cc55eb134ba3686c275b2820beadb8
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2024-10-23 18:44:26 +0200

    src/lua/oltp_inlist_update.lua: Add `reset_binlog` parameter

--- src/lua/oltp_inlist_update.lua
+++ src/lua/oltp_inlist_update.lua
@@ -13,10 +13,19 @@ sysbench.cmdline.options.random_points =
    {"Number of random points in the IN() clause in generated SELECTs", 10}
 sysbench.cmdline.options.hot_points =
    {"If true then use the same N adjacent values for the inlist clause", false}
+sysbench.cmdline.options.reset_binlog =
+   {"Number of UPDATEs to call RESET MASTER to remove binlogs", 0}
+
+mysql_version = 7.0
 
 function thread_init()
    drv = sysbench.sql.driver()
    con = drv:connect()
+
+   if sysbench.tid == 1 then
+      local result = con:query_row("SELECT VERSION()")
+      mysql_version = tonumber(result:match("^(%d+%.%d+)"))  -- Extract major and minor version
+   end
 
    stmt = {}
    params = {}
@@ -61,6 +70,8 @@ function thread_done()
    con:disconnect()
 end
 
+event_counter = 0
+
 function event()
    local tnum = sysbench.rand.uniform(1, sysbench.opt.tables)
 
@@ -82,4 +93,19 @@ function event()
    end
 
    stmt[tnum]:execute()
+
+   -- Execute RESET on MySQL every 1000000 events
+   if sysbench.opt.reset_binlog > 0 and sysbench.tid == 1 then
+     if event_counter * sysbench.opt.threads * sysbench.opt.random_points >= sysbench.opt.reset_binlog then
+       if mysql_version < 8.4 then
+         reset_query = "RESET MASTER"
+       else
+         reset_query = "RESET BINARY LOGS AND GTIDS"
+       end
+       print("Executing " .. reset_query .. " for MySQL " .. mysql_version)
+       con:query(reset_query)
+       event_counter = 0
+     end
+     event_counter = event_counter + 1
+   end
 end
